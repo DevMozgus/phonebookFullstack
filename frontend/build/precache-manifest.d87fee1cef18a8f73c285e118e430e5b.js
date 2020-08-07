e94665cdeae72e24526afc6205ca4a78a25a8bb7
nikolaki333 <nurlicic@gmail.com>
2020-08-07 17:34:56 +0200

    build update

---
+++ frontend/build/precache-manifest.d87fee1cef18a8f73c285e118e430e5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe8a4de3a35d8fdd1087fec712df89b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c67ea754315ff67689a",
+    "url": "/static/css/main.e77ad772.chunk.css"
+  },
+  {
+    "revision": "12764fe01bcebb82caaf",
+    "url": "/static/js/2.666a6545.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.666a6545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c67ea754315ff67689a",
+    "url": "/static/js/main.44d03e2d.chunk.js"
+  },
+  {
+    "revision": "713ad6055104c6629d23",
+    "url": "/static/js/runtime-main.504470cd.js"
+  }
+]);
